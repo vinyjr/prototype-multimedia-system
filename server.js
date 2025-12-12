8ce500f21928dd213bb1ba903f750de44f7f78a4
Vinicius Junior <viniciusjunior.dev@gmail.com>
2025-12-12 00:26:12 -0300

    feat: add server http

---
+++ server.js
@@ -0,0 +1,15 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "express";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "src")));
+
+app.listen(PORT, () => {
+	console.log(`HTTP server running at http://localhost:${PORT}`);
+});
